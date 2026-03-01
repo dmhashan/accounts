652fb0004a45410862292bfe5e7af5fa844a7c9b
Hashan Dewaraja <dmhashan@gmail.com>
2026-03-02 03:04:54 +0530

    feat: add product icon SVG and update layout files to use it as favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('images/product-icon.svg') }}">
     <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
