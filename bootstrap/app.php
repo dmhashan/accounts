00468d11507e8f3a33a465d87b8e93938249e60f
Hashan Dewaraja <dmhashan@gmail.com>
2026-04-11 02:51:49 +0530

    feat(cors): add CORS configuration for API endpoints

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
         $middleware->alias([
             'permission' => \App\Http\Middleware\CheckPermission::class,
             'pp.token'   => \App\Http\Middleware\ResolvePpToken::class,
